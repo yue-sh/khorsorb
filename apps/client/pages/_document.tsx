9dad2a290f80bf1355dec75082c22fbd932f84bd
_StarChaser <sc@yue.sh>
2023-01-29 09:39:40 +0700

    feat(client): dashboard

--- apps/client/pages/_document.tsx
+++ apps/client/pages/_document.tsx
@@ -5,12 +5,16 @@ import theme from '../libs/theme'
 export default function Document() {
 	return (
 		<Html lang="en">
-			<Head />
-			<body>
-				<ColorModeScript initialColorMode={theme.config.initialColorMode} />
-				<Main />
-				<NextScript />
-			</body>
-		</Html>
-	)
+			<Head >
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+					<link rel="preconnect" href="https://fonts.gstatic.com" />
+						<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai&display=swap" rel="stylesheet" />
+						</Head>
+						<body>
+							<ColorModeScript initialColorMode={theme.config.initialColorMode} />
+							<Main />
+							<NextScript />
+						</body>
+					</Html>
+					)
 }
